8683cf581abf408d305965e1d61445e14e325dd6
Huynh <nguyenviethuynh1996.vih@gmail.com>
2019-04-04 06:35:05 +0700

    fix nav cart

--- js/cart.js
+++ js/cart.js
@@ -4,6 +4,12 @@ if(document.readyState == 'loading'){
     ready()
 }
 
+function Shop(title, price, imageSrc){
+    this.title = title
+    this.price = price
+    this.imageSrc = imageSrc
+}
+
 function ready(){
     var addToCartButtons = document.getElementsByClassName('shop-item-button')
     for(var i = 0; i < addToCartButtons.length; i++){
@@ -11,19 +17,40 @@ function ready(){
         button.addEventListener('click', addToCarClicked)
     }
 
-    
 }
 
 function addToCarClicked(event){
-    
     var button = event
     var shopItem = button.path[7]
-    
     var title = shopItem.getElementsByClassName('cart-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('price-a')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('img-a')[0].src
-    console.log(title, price, imageSrc)
+    var price = shopItem.getElementsByClassName('price-a')[0].innerText.replace('$','')
     
-    event.preventDefault()    
+    var imageSrc = shopItem.getElementsByClassName('img-a')[0].src
+    // console.log(title, price, imageSrc)
+    
+    // Add item cart
+    // addItemCart(title, price, imageSrc)
+
+    const shop = new Shop(title, price, imageSrc)
+
+    // addCartInLocalStorage
+    addCartInLocalStorage(shop)
+    
+
 }
+
+// save local storage
+function addCartInLocalStorage(shop){
+    let listsData 
+    if (localStorage.getItem('listsData') === null) {
+        listsData = []
+    } else {
+        listsData = JSON.parse(localStorage.getItem('listsData'))
+    }
+
+    listsData.push(shop)
+    localStorage.setItem('listsData', JSON.stringify(listsData))  
+    console.log('save ok')  
+}
+
 
